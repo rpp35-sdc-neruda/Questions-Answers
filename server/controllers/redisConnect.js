ba9a0033790ee59558b60eab886b9dd452597979
Shengnan Huang <selinahuang205@gmail.com>
2022-08-03 20:36:11 -0500

    fixed host for redis

--- server/controllers/redisConnect.js
+++ server/controllers/redisConnect.js
@@ -1,6 +1,6 @@
 //import { createClient } from 'redis';
 const Redis = require('redis');
-const redisClient = Redis.createClient();
+const redisClient = Redis.createClient({host: '52.91.205.48'});
 
 //redisClient.on('connect', () => console.log('Hi redis'));
 redisClient.on('error', (err) => console.log('Redis Client Error', err));
