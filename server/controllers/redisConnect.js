e845f3e3c83e06beb4429eedeb34fc47d51d7e65
Shengnan Huang <selinahuang205@gmail.com>
2022-08-04 17:00:51 -0500

    change redis connection

--- server/controllers/redisConnect.js
+++ server/controllers/redisConnect.js
@@ -1,8 +1,8 @@
 
 const Redis = require('redis');
-//const redisClient = Redis.createClient({host: '52.91.205.48'});
+const redisClient = Redis.createClient({host: '52.91.205.48'});
 
-const redisClient = Redis.createClient();
+//const redisClient = Redis.createClient();
 //redisClient.on('connect', () => console.log('Hi redis'));
 redisClient.on('error', (err) => console.log('Redis Client Error', err));
 
