eb0bb2fe9327830bb17f0988a71ac35815d8b134
Shengnan Huang <selinahuang205@gmail.com>
2022-08-05 20:50:27 -0500

    redis on seperate instance

--- server/controllers/redisConnect.js
+++ server/controllers/redisConnect.js
@@ -1,6 +1,6 @@
 
 const Redis = require('redis');
-const redisClient = Redis.createClient({host: '18.207.244.107'});
+const redisClient = Redis.createClient({host: '3.87.14.99'});
 
 //const redisClient = Redis.createClient();
 //redisClient.on('connect', () => console.log('Hi redis'));
